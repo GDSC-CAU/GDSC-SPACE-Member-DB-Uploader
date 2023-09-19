ecd0eb836d925a1d03652b9bde45be47490e3a35
yymin1022 <yymin1022@gmail.com>
2023-09-19 15:32:16 +0900

    Update main.py : Added Basic DB Upload Code

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+cred = credentials.Certificate('serviceAccount.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
+load_dotenv()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+data = {
+    "Behance": "",
+    "Comment": "",
+    "Email": "",
+    "Gender": "",
+    "Github": "",
+    "Image": "",
+    "Instagram": "",
+    "Name": "",
+    "Nickname": "",
+    "Position": "",
+    "Role": ""
+}
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+member_ref = db.collection("Members").document("2")
+member_ref.update({"list": firestore.ArrayUnion([data])})
